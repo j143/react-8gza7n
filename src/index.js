43b447a6c4369decb80e8dc1cad4b5fd460f37a4
j143 <j143@protonmail.com>
2025-04-01 09:12:22 +0530

    now the styles are loaded properly

--- src/index.js
+++ src/index.js
@@ -4,14 +4,13 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import CPUArchitectureAnimation from './CPUArchitecture';
 
-import './style.css';
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
     <CPUArchitectureAnimation />
   </StrictMode>
 );
